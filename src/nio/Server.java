5f90d05190d1058140a700e63734207da36bb1ce
liubing <576192031@qq.com>
2018-11-05 23:44:52 +0800

    <fix>：nio笔记增加

---
+++ src/nio/Server.java
@@ -0,0 +1,15 @@
+package nio;
+/*
+ *一句话描述该类作用:【】
+ *@Author:LB
+ *
+ */
+
+public class Server {
+
+    private static int DEFAULT_PORT = 8888;
+
+    private static ServerHandle serverHandle;
+
+
+}
